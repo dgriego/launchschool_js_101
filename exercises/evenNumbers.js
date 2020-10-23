5c408e9a5feb6b487aa10f13a58df7f907301c10
dgriego <eggomygriego@gmail.com>
2020-10-22 21:29:39 -0700

    add evenNumbers solution

---
+++ exercises/evenNumbers.js
@@ -0,0 +1,8 @@
+// Log all even numbers from 1 to 99, inclusive, to the console.
+// Log all numbers on separate lines.
+
+for (let i = 1; i < 100; i += 1) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
