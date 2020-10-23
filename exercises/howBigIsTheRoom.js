0441ffdce0786f4d2ffc6b7038a9d8515e2a708f
dgriego <eggomygriego@gmail.com>
2020-10-22 21:39:48 -0700

    add howBigIsTheRoom solution file

---
+++ exercises/howBigIsTheRoom.js
@@ -0,0 +1,9 @@
+// Build a program that asks the user to enter the length and width
+// of a room in meters, and then logs the area of the room to the
+// console in both square meters and square feet.
+
+// Note: 1 square meter == 10.7639 square feet
+
+const ReadlineSync = require('readline-sync');
+
+console.log(Readline);
