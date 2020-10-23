9c04a68505cbe37f0c751fa208b532e53582d340
dgriego <eggomygriego@gmail.com>
2020-10-23 13:21:46 -0700

    add sumOfProduct full solution

--- exercises/sumOfProduct.js
+++ exercises/sumOfProduct.js
@@ -13,10 +13,36 @@
 // if product then I will increment with multiplication
 
 const ReadlineSync = require('readline-sync');
+const SUM_TEXT = 's';
+const PRODUCT_TEXT = 'p';
 
-let initial_int = 0;
-
-while (initial_int <= 0) {
+let user_int = 0;
+while (user_int <= 0) {
   console.log('Please enter an integer greater than 0:');
-  initial_int = parseInt(ReadlineSync.prompt());
+  user_int = parseInt(ReadlineSync.prompt());
 }
+
+let compute_type = '';
+while (compute_type !== SUM_TEXT && compute_type !== PRODUCT_TEXT) {
+  console.log('Enter "s" to compute the sum, or "p" to compute the product.');
+  compute_type = ReadlineSync.prompt();
+}
+
+let int_arr = [];
+let incrementer = 1;
+while (incrementer <= user_int) {
+  int_arr.push(incrementer);
+  incrementer += 1;
+}
+
+let computed_value = 0;
+if (compute_type === SUM_TEXT) {
+  computed_value = int_arr.reduce((acc, val) => acc + val);
+} else {
+  computed_value = int_arr.reduce((acc, val) => acc * val);
+}
+
+const computed_type_string = compute_type === SUM_TEXT ? 'sum' : 'product';
+console.log(
+  `The ${computed_type_string} of the integers between 1 and ${user_int} is ${computed_value}.`
+)
