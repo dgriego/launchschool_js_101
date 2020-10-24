e901c51b002e7bc8980fd936578e59a1f74e3761
dgriego <eggomygriego@gmail.com>
2020-10-23 17:56:42 -0700

    add walkthrough solution for calculator

---
+++ lesson_2/calculator.js
@@ -0,0 +1,32 @@
+// ask the user for the first number
+// parse the string as a float
+// ask the user for the second number
+// parse the string as a float
+// ask for the operation to perform
+// perform the calculation and display result to the user
+
+const readline = require('readline-sync');
+
+console.log('Welcome to Calculator!');
+
+console.log("What's the first number?");
+let number1 = Number(readline.question());
+
+console.log("What's the second number?");
+let number2 = Number(readline.question());
+
+console.log('What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide');
+let operation = readline.question();
+
+let output;
+if (operation === '1') {
+  output = number1 + number2;
+} else if (operation === '2') {
+  output = number1 - number2;
+} else if (operation === '3') {
+  output = number1 * number2;
+} else if (operation === '4') {
+  output = number1 / number2;
+}
+
+console.log(`The result is: ${output}`);
