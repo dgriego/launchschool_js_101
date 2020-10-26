4b6404db0431b1c53325822e6993b471346a2398
dgriego <eggomygriego@gmail.com>
2020-10-26 01:23:37 -0700

    add loan amount functionality and validation for float input

--- lesson_2/mortgageCalculator.js
+++ lesson_2/mortgageCalculator.js
@@ -1,4 +1,26 @@
 const readline = require('readline-sync');
 const messages = require('./mortgage_calculator_messages.json');
 
+const prompt = (text) => {
+  console.log(`-> ${text}`);
+};
+
+const validFloat = (float) => Number.isNaN(parseFloat(float));
+
+const getValidFloatFromUser = () => {
+  let float = parseFloat(readline.question());
+  while (validFloat(float)) {
+    console.log("Hmm... that doesn't look like a valid number");
+    float = parseFloat(readline.question());
+  }
+
+  return float.toFixed(2);
+};
+
 console.log(messages['title']);
+
+prompt(messages['questions']['loan_amount']);
+const loanAmount = getValidFloatFromUser();
+
+console.log(loanAmount);
+
