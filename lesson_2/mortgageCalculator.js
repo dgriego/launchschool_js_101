84662f6d3af9ba02515ec420fa011e524919fa26
dgriego <eggomygriego@gmail.com>
2020-10-26 01:34:31 -0700

    Add term length questions
    - add error message for invalid number

--- lesson_2/mortgageCalculator.js
+++ lesson_2/mortgageCalculator.js
@@ -6,22 +6,38 @@ const prompt = (text) => {
 };
 
 const validFloat = (float) => Number.isNaN(parseFloat(float));
+const validNumber = (float) => Number.isNaN(parseInt(float, 10));
 
 const getValidFloatFromUser = () => {
   let float = parseFloat(readline.question());
   while (validFloat(float)) {
-    console.log("Hmm... that doesn't look like a valid number");
+    console.log(messages['error']['invalid_number']);
     float = parseFloat(readline.question());
   }
 
   return float.toFixed(2);
 };
 
+const getValidNumberFromUser = () => {
+  let number = parseInt(readline.question(), 10);
+  while (validNumber(number)) {
+    console.log(messages['error']['invalid_number']);
+    number = parseInt(readline.question(), 10);
+  }
+
+  return number;
+};
+
 console.log(messages['title']);
 
-prompt(messages['questions']['loan_amount']);
+prompt(messages['question']['loan_amount']);
 const loanAmount = getValidFloatFromUser();
 
-prompt(messages['questions']['apr']);
+prompt(messages['question']['apr']);
 const apr = getValidFloatFromUser();
 
+prompt(messages['question']['loan_duration_years']);
+const loanDurationYears = getValidNumberFromUser();
+
+prompt(messages['question']['loan_duration_months']);
+const loanDurationMonths = getValidNumberFromUser();
