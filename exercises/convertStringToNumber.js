8832aa4d7ca4178080677bfe41f976f15b107b9a
dgriego <eggomygriego@gmail.com>
2020-10-27 07:52:53 -0700

    add stringToSignedNumber exercise

--- exercises/convertStringToNumber.js
+++ exercises/convertStringToNumber.js
@@ -1,51 +1,54 @@
+/* eslint-disable max-lines-per-function */
 // need a dictionary that will store the string representation of a number
 // as the key
 
 function stringToInteger(intStr) {
   const stringToNumRef = {
-    '0': 0,
-    '1': 1,
-    '2': 2,
-    '3': 3,
-    '4': 4,
-    '5': 5,
-    '6': 6,
-    '7': 7,
-    '8': 8,
-    '9': 9
-  }
+    0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9
+  };
 
-  // creat initial var to store int
   let int = 0;
 
-  // convert intStr to array of chars
   const strArr = intStr.split('');
-
-  // depending on length of string I want to ensure I'm placing the number
-  // at the proper position
-  // if the int is 4352
-  // 10 is base value to determine position
-  // if position is 4, 10 * 4
-
-  // index 0 - end of arr
-  // 10 multiplier to go from highest position to lowest
-  //
-  // string length = 4
-  // 0, 1000 (10 ** 3)
-  // 1, 100 (10 ** 2)
-  // 2, 10 (10 ** 1)
-  // 4, 1 (10 ** 0)
 
   let decrementer = strArr.length - 1;
 
-  for (let i = 0; i <= strArr.length - 1; i += 1) {
+  for (let idx = 0; idx <= strArr.length - 1; idx += 1) {
     let posMultiplier = 10 ** decrementer;
-    int += posMultiplier * stringToNumRef[strArr[i]];
+    int += posMultiplier * stringToNumRef[strArr[idx]];
     decrementer -= 1;
   }
 
   return int;
 }
 
-console.log(stringToInteger("4321") === 4321); // logs true
-console.log(stringToInteger("570") === 570); // logs true
+// need a dictionary that will store the string representation of a number
+// as the key
+
+// eslint-disable-next-line max-lines-per-function
+// eslint-disable-next-line max-statements
+function stringToSignedInteger(intStr) {
+  const stringToNumRef = {
+    0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9
+  };
+
+  let int = 0;
+  const strArr = intStr.split('');
+  let sign = strArr.shift();
+  let decrementer = strArr.length - 1;
+
+  for (let idx = 0; idx <= strArr.length - 1; idx += 1) {
+    let posMultiplier = 10 ** decrementer;
+    int += posMultiplier * stringToNumRef[strArr[idx]];
+    decrementer -= 1;
+  }
+
+  if (sign === '-') {
+    int = -int;
+  }
+
+  return int;
+}
+
+console.log(stringToSignedInteger("+4321") === 4321); // logs true
+console.log(stringToSignedInteger("-570") === -570); // logs true
