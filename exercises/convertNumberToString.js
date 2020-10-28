7dd6812e60b851fa1e79d2e0af83757abb4d8544
dgriego <eggomygriego@gmail.com>
2020-10-28 10:08:51 -0700

    add signed integer exercise

--- exercises/convertNumberToString.js
+++ exercises/convertNumberToString.js
@@ -1,16 +1,25 @@
+/* eslint-disable max-lines-per-function */
 // one method
 // determine size of number first to know what 10 to start off at
 
 // if number is 4057
 
-function integerToString(number) {
+function signedIntegerToString(number) {
   const numberToStringRef = {
     0: '0', 1: '1', 2: '2', 3: '3', 4: '4', 5: '5', 6: '6', 7: '7', 8: '8', 9: '9'
   };
 
   let incrementer = 0;
   let numberArr = [];
+  let sign = '';
 
+  if (number > 0) {
+    sign = '+';
+  } else if (number < 0) {
+    sign = '-';
+  }
+
+  number = Math.abs(number);
   while (true) {
     incrementer += 1;
     let quotient = parseInt(number / (10 ** incrementer), 10);
@@ -26,10 +35,13 @@ function integerToString(number) {
   const lastNumber = numberToStringRef[number % 10];
   numberArr.push(lastNumber);
 
+  if (sign) {
+    numberArr.unshift(sign);
+  }
+
   return numberArr.join('');
 }
 
-integerToString(4321);      // "4321"
-integerToString(0);         // "0"
-integerToString(5000);      // "5000"
-integerToString(1234567890);      // "1234567890"
+console.log(signedIntegerToString(4321) === "+4321");
+console.log(signedIntegerToString(-123) === "-123");
+console.log(signedIntegerToString(0) === "0");
